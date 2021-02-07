dedf1f9521c1702c7a560b55b30f1376b9b8a5f9
rameshwar setty <rameshwarshetty987@gmail.com>
2021-02-07 19:41:22 +0530

    flightapp created

---
+++ FlightReservation/src/main/java/com/histricks/apps/entites/Passenger.java
@@ -0,0 +1,20 @@
+package com.histricks.apps.entites;
+
+import lombok.Data;
+
+@Data
+public class Passenger {
+	
+	private Long id;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private String middleName;
+	
+	private String email;
+	
+	private String phone;
+
+}
